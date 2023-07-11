57dbfc49e803897dc25a3b21d0d8892f3989918a
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-07-11 16:09:22 +0700

    Update 2023_07_04_042337_create_customers_table.php

--- database/migrations/2023_07_04_042337_create_customers_table.php
+++ database/migrations/2023_07_04_042337_create_customers_table.php
@@ -13,7 +13,7 @@ class CreateCustomersTable extends Migration
      */
     public function up()
     {
-        Schema::table('customers', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
             $table->integer('user_id')->nullable();
