27a9f50a279f53eb9d15948a9f7083b5a63173b4
lucky <54930672+luxlnw171041@users.noreply.github.com>
2023-07-11 12:05:06 +0700

    .

---
+++ database/migrations/2023_07_04_042337_create_customers_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user_id')->nullable();
+            $table->string('rentname')->nullable();
+            $table->string('compname')->nullable();
+            $table->string('c_name')->nullable();
+            $table->string('c_surname')->nullable();
+            $table->string('c_idno')->nullable();
+            $table->string('demerit')->nullable();
+            $table->string('demeritdetail')->nullable();
+            $table->string('c_pic_id_card')->nullable();
+            $table->string('c_pic_lease')->nullable();
+            $table->string('c_pic_execution')->nullable();
+            $table->string('c_pic_cap')->nullable();
+            $table->string('c_pic_other')->nullable();
+            $table->string('c_date')->nullable();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('customers');
+    }
+}
